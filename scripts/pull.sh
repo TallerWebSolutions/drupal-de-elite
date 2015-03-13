c5bdd824564891db4f713a2ca29eab37a4818617
Denis Souza <denis@merchannew.com.br>
2015-03-13 12:42:21 -0300

    Created pull.sh file.

---
+++ scripts/pull.sh
@@ -0,0 +1,37 @@
+if [[ "$1" == "" ]]; then
+  BRANCH="dev"
+else
+  BRANCH="$1"
+fi
+OUTPUT=$(pwd)
+DIRS=(${OUTPUT//// })
+LENGTH=${#DIRS[@]}
+LAST_POSITION=$((LENGTH - 1)) # Subtract 1 from the length.
+LAST_PART=${DIRS[${LAST_POSITION}]}
+if [[ "$LAST_PART" == "docroot" ]]; then
+  MSG='Checkoutizinho para garantir que está no branch '
+  MSG+="$BRANCH"
+  MSG+=' ...'
+  echo "$MSG"
+  #rm -Rf sites/default/config
+  #git checkout sites/default/config
+  git checkout "$BRANCH"
+  echo 'Trazendo as atualizações do branch remoto...'
+  git pull origin "$BRANCH"
+  echo 'Executando possíveis atualizações no Drupal...'
+  drush updb -y
+  echo 'Sincronizando o CM com seu banco de dados local..'
+  #drush config-sync
+  #echo 'Recuperando a pasta config novamente para te manter sincronizado...'
+  #rm -Rf sites/default/config
+  #git checkout sites/default/config
+  git status
+  #cd sites/all/themes/theme829
+  #bower install
+  #cd sass
+  #compass clean && compass compile
+  drush cc all
+  echo 'Parabéns! Pode acabar com a reza que tudo foi bem! :-)'
+else
+  echo "Execute esse comando no diretório docroot"
+fi
