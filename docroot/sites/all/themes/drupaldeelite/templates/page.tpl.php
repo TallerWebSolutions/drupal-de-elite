1b3fbe4b571324d06583e51e5321838c0da8c5c5
wlsf82 <lhguerra@gmail.com>
2015-05-04 11:47:07 -0300

    Removed gridsystem classes from template.

--- docroot/sites/all/themes/drupaldeelite/templates/page.tpl.php
+++ docroot/sites/all/themes/drupaldeelite/templates/page.tpl.php
@@ -33,8 +33,8 @@
 
   <?php if (!empty($page['featured'])): ?>
     <!--/.featured -->
-    <section class="l-featured row">
-      <div class="large-12 columns">
+    <section class="l-featured">
+      <div>
         <?php print render($page['featured']); ?>
       </div>
     </section>
@@ -43,8 +43,8 @@
 
   <?php if ($messages && !$zurb_foundation_messages_modal): ?>
     <!--/.l-messages -->
-    <section class="l-messages row">
-      <div class="large-12 columns">
+    <section class="l-messages">
+      <div>
         <?php if ($messages): print $messages; endif; ?>
       </div>
     </section>
@@ -53,16 +53,16 @@
 
   <?php if (!empty($page['help'])): ?>
     <!--/.l-help -->
-    <section class="l-help row">
-      <div class="large-12 columns">
+    <section class="l-help">
+      <div>
         <?php print render($page['help']); ?>
       </div>
     </section>
     <!--/.l-help -->
   <?php endif; ?>
 
-  <main role="main" class="row l-main">
-    <div class="<?php //print $main_grid; ?> main columns">
+  <main role="main" class="l-main">
+    <div class="main">
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlight panel callout">
           <?php print render($page['highlighted']); ?>
@@ -95,72 +95,68 @@
     <!--/.main region -->
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar">
+      <aside role="complementary" class="sidebar-first sidebar">
         <?php print render($page['sidebar_first']); ?>
       </aside>
     <?php endif; ?>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside role="complementary" class="<?php //print $sidebar_sec_grid; ?> sidebar-second columns sidebar">
+      <aside role="complementary" class="sidebar-second sidebar">
         <?php print render($page['sidebar_second']); ?>
       </aside>
     <?php endif; ?>
   </main>
   <!--/.main-->
 
-  <?php if (!empty($page['triptych_first']) || !empty($page['triptych_middle']) || !empty($page['triptych_last'])): ?>
-    <!--.triptych-->
+<!--   </?php if (!empty($page['triptych_first']) || !empty($page['triptych_middle']) || !empty($page['triptych_last'])): ?>
     <section class="l-triptych row">
       <div class="triptych-first large-4 columns">
-        <?php print render($page['triptych_first']); ?>
+        </?php print render($page['triptych_first']); ?>
       </div>
       <div class="triptych-middle large-4 columns">
-        <?php print render($page['triptych_middle']); ?>
+        </?php print render($page['triptych_middle']); ?>
       </div>
       <div class="triptych-last large-4 columns">
-        <?php print render($page['triptych_last']); ?>
+        </?php print render($page['triptych_last']); ?>
       </div>
     </section>
-    <!--/.triptych -->
-  <?php endif; ?>
-
-  <?php if (!empty($page['footer_firstcolumn']) || !empty($page['footer_secondcolumn']) || !empty($page['footer_thirdcolumn']) || !empty($page['footer_fourthcolumn'])): ?>
-    <!--.footer-columns -->
+  </?php endif; ?> -->
+<!--
+  </?php if (!empty($page['footer_firstcolumn']) || !empty($page['footer_secondcolumn']) || !empty($page['footer_thirdcolumn']) || !empty($page['footer_fourthcolumn'])): ?>
     <section class="row l-footer-columns">
-      <?php if (!empty($page['footer_firstcolumn'])): ?>
+      </?php if (!empty($page['footer_firstcolumn'])): ?>
         <div class="footer-first large-3 columns">
-          <?php print render($page['footer_firstcolumn']); ?>
+          </?php print render($page['footer_firstcolumn']); ?>
         </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_secondcolumn'])): ?>
+      </?php endif; ?>
+      </?php if (!empty($page['footer_secondcolumn'])): ?>
         <div class="footer-second large-3 columns">
-          <?php print render($page['footer_secondcolumn']); ?>
+          </?php print render($page['footer_secondcolumn']); ?>
         </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_thirdcolumn'])): ?>
+      </?php endif; ?>
+      </?php if (!empty($page['footer_thirdcolumn'])): ?>
         <div class="footer-third large-3 columns">
-          <?php print render($page['footer_thirdcolumn']); ?>
+          </?php print render($page['footer_thirdcolumn']); ?>
         </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_fourthcolumn'])): ?>
+      </?php endif; ?>
+      </?php if (!empty($page['footer_fourthcolumn'])): ?>
         <div class="footer-fourth large-3 columns">
-          <?php print render($page['footer_fourthcolumn']); ?>
+          </?php print render($page['footer_fourthcolumn']); ?>
         </div>
-      <?php endif; ?>
+      </?php endif; ?>
     </section>
-    <!--/.footer-columns-->
-  <?php endif; ?>
-
+  </?php endif; ?>
+ -->
   <!--.l-footer-->
-  <footer class="l-footer panel row" role="contentinfo">
+  <footer class="l-footer panel" role="contentinfo">
     <?php if (!empty($page['footer'])): ?>
-      <div class="footer large-12 columns">
+      <div class="footer">
         <?php print render($page['footer']); ?>
       </div>
     <?php endif; ?>
 
     <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
+      <div class="copyright">
         &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
       </div>
     <?php endif; ?>
