135d9702e76cc715de23b2af0515b1d20c4c6fcb
wlsf82 <lhguerra@gmail.com>
2015-05-14 12:56:48 -0300

    Added custom node display with color invert button.

---
+++ docroot/sites/all/themes/drupaldeelite/templates/ds-1col--node-full.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+
+
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
+
+  <a class="invert-colors">Inverter cores</a>
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php print $ds_content; ?>
+</<?php print $ds_content_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
