36b39b6ee9c8b84524a4a30111539340527ec7fe
wlsf82 <lhguerra@gmail.com>
2015-05-15 18:35:01 -0300

    Added new cool button hover effect.

--- docroot/sites/all/themes/drupaldeelite/js/drupaldeelite.js
+++ docroot/sites/all/themes/drupaldeelite/js/drupaldeelite.js
@@ -3,7 +3,7 @@
   // Invert colors in internal post pages.
   Drupal.behaviors.invertColors = {
     attach: function(context, settings) {
-      var $invertButton = $('.invert-colors'),
+      var $invertButton = $('.invert-colors', context),
           $page = $('.page');
 
       $invertButton.on('click', function(e) {
--- docroot/sites/all/themes/drupaldeelite/templates/ds-1col--node-full.tpl.php
+++ docroot/sites/all/themes/drupaldeelite/templates/ds-1col--node-full.tpl.php
@@ -9,7 +9,9 @@
 
 <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
 
-  <a class="invert-colors">Inverter cores</a>
+  <span class="invert-colors-wrapper">
+    <a class="invert-colors">Inverter cores</a>
+  </span>
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
